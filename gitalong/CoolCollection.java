9d4f8d6e292d5d20bdc9b951883d3f694ac8ac22
devEvanH <122240154+devEvanH@users.noreply.github.com>
2023-05-01 01:31:30 +0000

    Made the WellBehavedIterator class with the iterator variable and the hasNext() that checks if there is another value to iterate to which returns a boolean and a method called next() that returns the next element if it exists, if not it throws an IllegalStateException.

--- gitalong/CoolCollection.java
+++ gitalong/CoolCollection.java
@@ -38,11 +38,13 @@ public class CoolCollection<E> {
         return this.pieces.size();
     }
 
+    // How in the world do I use the iterator interface for this
      private class RandomIterator<T> implements Iterator<T> {
         private Random random;
         private int[] indexOrder;
         private int currentIndex; 
 
+       // Instead of doing rand in constructor do I still do the count but store each unique num and add it to the array each time and then use iterator that way
         public RandomIterator(int seed) {
             this.iterator = pieces.iterator();
             this.random = new Random(seed);
@@ -77,5 +79,21 @@ public class CoolCollection<E> {
         }
 
      }
+  
+  private class WellBehavedIterator<T> implements Iterator<T> {
+    private Iterator<T> iterator;
+
+    public boolean hasNext() {
+        return this.iterator.hasNext();
+    }
+
+    public T next() {
+        if (!hasNext()) {
+            throw new IllegalStateException("No more elements to iterate");
+        }
+        T element = this.iterator.next();
+        return element;
+    }
+  }
 
 }
