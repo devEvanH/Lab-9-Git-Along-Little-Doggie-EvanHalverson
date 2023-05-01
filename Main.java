4c52b746fed44e3b2fba2b5b3ecf0851d91b5c25
devEvanH <122240154+devEvanH@users.noreply.github.com>
2023-05-01 03:14:32 +0000

    I added the comments and reflection and a try-catch for the IllegalStateException that I threw earlier

--- Main.java
+++ Main.java
@@ -2,34 +2,40 @@
 *
 * Name: Evan Halverson
 * Date: 4/28/2023
-* Description: I don't know
-* Bugs: All of them
-* Reflection: 
+* Description: This code creates two instances of the CoolCollection class, one with the option to iterate randomly and another with the option to iterate well-behaved. The collections are populated with a series of words using the add method, and then an iterator is obtained for each collection using the iterator method. The contents of each collection are printed out using the next method of the iterator. The purpose of the code is to showcase the different iteration behaviors of the CoolCollection class.
+* Bugs: None
+* Reflection: I found this lab to to be alright. The random iterator was a bit confusing and seemed like an odd thing to do, but I got the job. Not a bad lab, just kind of a weird one.  
 *
 */
 import java.util.*;
 import gitalong.*;
   
 class Main {
+  // This is the main method of the program.
+  // It creates two instances of the CoolCollection class, one that iterates randomly and another that iterates in a well-behaved manner.
   public static void main(String[] args) {
+    // Create a CoolCollection instance with the option to iterate randomly.
     CoolCollection<String> randomCollection = new CoolCollection<>(true);
-
+    // Add some words to the collection.
     String[] words = {"never", "trust", "an", "atom,", "they", "make", "up", "everything"};
     for (String word : words) {
         randomCollection.add(word);
     }
-
+    // Get an iterator for the collection and print out its contents.
     Iterator<String> iterator = randomCollection.iterator();
     System.out.print("Random: ");
     while (iterator.hasNext()) {
       System.out.print(iterator.next() + " ");
     }
     System.out.println();
-
+    System.out.println();
+    // Create another CoolCollection instance with the option to iterate in a well-behaved manner.
     CoolCollection<String> wellBehavedCollection = new CoolCollection<>(false);
+    // Add the same words to this collection.
     for (String word : words) {
         wellBehavedCollection.add(word);
     }
+    // Get an iterator for this collection and print out its contents.
     Iterator<String> wellBehavedIterator = wellBehavedCollection.iterator();
     System.out.print("Well Behaved Iterator: ");
     while (wellBehavedIterator.hasNext()) {
--- gitalong/CoolCollection.java
+++ gitalong/CoolCollection.java
@@ -1,59 +1,135 @@
 package gitalong;
 import java.util.*;
 
+/**
+*
+* Name: Evan Halverson
+* Date: 4/28/2023
+* Description: This code defines a custom implementation of a collection class called CoolCollection, which can iterate either randomly or in a well-behaved manner. Two instances of this class are created in the code, one with the random iteration option and the other with the well-behaved iteration option. 
+* Bugs: None
+* Reflection: I made the random version of the code without using the iterated because I thought the iterator was unnessecary, but then I refactor it to what the directions asked for. The other inner class was incredibley easy after that. I would have rather started with the basic iterator before moving onto the more complex version. 
+*
+*/
+
+
+/**
+ * A custom implementation of a collection, which can iterate randomly or in a well-behaved manner.
+ *
+ * @param iterateRandomly a boolean value indicating whether the collection should iterate randomly or in a well-behaved manner.
+ */
 public class CoolCollection<E> {
     private ArrayList<E> pieces;
     private boolean iterateRandomly;
     
-
+    /**
+     * Constructs a CoolCollection with the specified iteration mode.
+     *
+     * @param iterateRandomly a boolean value indicating whether the collection should iterate randomly or in a well-behaved manner.
+     */
     public CoolCollection(boolean iterateRandomly) {
         this.pieces = new ArrayList<E>();
         this.iterateRandomly = iterateRandomly;
     }
 
+    /**
+     * Adds the specified element to the collection.
+     *
+     * @param element the element to be added
+     * @return true if the element was added to the collection, false otherwise
+     */
     public boolean add(E element) {
         return this.pieces.add(element);
     }
 
+    /**
+     * Adds the specified element at the specified position in the collection.
+     *
+     * @param pos the position at which to add the element
+     * @param element the element to be added
+     */
     public void add(int pos, E element) {
         this.pieces.add(pos, element);
     }
 
+    /**
+     * Removes the specified element from the collection.
+     *
+     * @param element the element to be removed
+     */
     public void remove(E element) {
         this.pieces.remove(element);
     }
 
+    /**
+     * Removes the element at the specified position in the collection.
+     *
+     * @param pos the position of the element to be removed
+     * @return the removed element
+     */
     public E remove(int pos) {
         return this.pieces.remove(pos);
     }
 
-    
+    /**
+     * Returns an iterator over the elements in the collection.
+     * @return an iterator over the elements in the collection
+     * @throws IllegalStateException if an error occurs while creating the iterator
+     */ 
     public Iterator<E> iterator() {
-        if (this.iterateRandomly) {
-            return new RandomIterator(1234567);
-        } else {
-            return new WellBehavedIterator();
-        }
+      try {
+          if (this.iterateRandomly) {
+              return new RandomIterator(1234567);
+          } else {
+              return new WellBehavedIterator();
+          }
+      } catch (IllegalStateException e) {
+          System.err.println("An error occurred while creating the iterator: " + e.getMessage());
+          System.exit(1);
+          return null; // Only here to satisfy the compiler
+      }
     }
-
+  
+    /**
+     * Returns the number of elements in the collection.
+     *
+     * @return the number of elements in the collection
+     */
     public int size() {
         return this.pieces.size();
     }
-  
-    private class RandomIterator implements Iterator<E> {
-    private Random random;
-    private Set<Integer> usedIndexes;
 
-    public RandomIterator(int seed) {
+    /**
+     * A private inner class that implements the RandomIterator.
+     */
+    private class RandomIterator implements Iterator<E> {
+      private Random random;
+      private Set<Integer> usedIndexes;
+
+      /**
+       * Constructs a RandomIterator with the specified seed.
+       *
+       * @param seed the seed for the random number generator
+       */
+      public RandomIterator(int seed) {
         this.random = new Random(seed);
         this.usedIndexes = new HashSet<Integer>();
-    }
+      }
 
-    public boolean hasNext() {
+      /**
+       * Returns true if there are more elements to iterate over, false otherwise.
+       *
+       * @return true if there are more elements to iterate over, false otherwise
+       */
+      public boolean hasNext() {
         return usedIndexes.size() < pieces.size();
-    }
+      }
 
-    public E next() {
+      /**
+       * Returns the next random element in the collection.
+       *
+       * @return the next random element in the collection
+       */
+      public E next() {
         if (!hasNext()) {
             throw new IllegalStateException("No more elements to iterate");
         }
@@ -65,21 +141,35 @@ public class CoolCollection<E> {
 
         usedIndexes.add(index);
         return pieces.get(index);
+      }
     }
-  }
 
-  
+  /**
+   * A private inner class that implements the WellBehavedIterator.
+   */
   private class WellBehavedIterator implements Iterator<E> {
     private Iterator<E> iterator;
 
+    /**
+     * Constructs a new WellBehavedIterator. 
+     */
     public WellBehavedIterator() {
         this.iterator = pieces.iterator(); 
     }
 
+    /**
+     * Returns true if there is another element to iterate over, and false otherwise.
+     * @return true if there is another element to iterate over, and false otherwise.
+     */
     public boolean hasNext() {
         return this.iterator.hasNext();
     }
 
+    /**
+     * Returns the next element in the iteration.
+     * @return the next element in the iteration.
+     * @throws IllegalStateException if there is no next element to iterate over.
+     */
     public E next() {
         if (!hasNext()) {
             throw new IllegalStateException("No more elements to iterate");
